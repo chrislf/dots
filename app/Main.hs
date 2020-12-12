3e282df0559f25d4b6aec56196a2b8b788a6550b
chrislf <chrislf@freeshell.org>
2020-12-12 17:24:59 +0000

    arrays, animate

--- app/Main.hs
+++ app/Main.hs
@@ -1,25 +1,65 @@
 module Main where
 
 import Graphics.Gloss
+import Data.Function (on)
 
 import Lib
 
+windowSize :: Int
+windowSize = 300
 
 glossMain :: IO ()
-glossMain = display (InWindow "Window Name" (300, 300) (10, 10)) white pic
+--glossMain = display (InWindow "Window Name" (windowSize, windowSize) (10, 10)) white (showArray a)
+glossMain = animate (InWindow "Window Name" (windowSize, windowSize) (10, 10)) white (\t -> showArray (animateArray t a))
 
 
+data LED = LED {
+    colour :: Color
+  , lit :: Bool
+}
+
+showLED :: LED -> Picture
+showLED (LED colour lit) = Color colour l
+  where 
+    l = if lit
+          then ThickCircle 5 10
+          else Circle 10
+
+type Array = [[LED]]
 
 
-pic = mconcat [ translate 0 (25*fromIntegral n) pp | n <- [-5..5] ]
-  where
-    pp = mconcat [ Color (fc n) (translate (25*fromIntegral n) 0 p) | n <- [-5..5] ]
-    p = ThickCircle 5 10
-    fc x
-      | abs (x `rem` 3) == 0 = red
-      | abs (x `rem` 3) == 1 = green
-      | abs (x `rem` 3) == 2 = blue
-      | otherwise = error (show x)
+rll = LED red True
+gll = LED green True
+bll = LED blue True
+
+a = [
+    [ rll, rll, rll, rll ]
+  , [ gll, gll, gll, gll ]
+  , [ bll, bll, bll, bll ]
+  , [ rll, gll, bll, rll ]
+  ]
+
+
+animateArray :: Float -> Array -> Array
+animateArray f = take (round f)
+
+showArray :: Array -> Picture
+showArray xss = result
+  where 
+    width = length . head $ xss
+    height = length xss
+    halfWidth = width `fdiv` 2
+    halfHeight = height `fdiv` 2
+    wRange = [-halfWidth..halfWidth]
+    hRange = [-halfHeight..halfHeight]
+
+    makeRow r = mconcat $ zipWith (\x l -> translate (25*x) 0 (showLED l)) wRange r
+    rows = map makeRow xss
+    result = mconcat $ zipWith (\y p -> translate 0 (25*y) p) hRange rows
+
+-- why can't we write Integral a, b?
+fdiv :: Fractional c => Int -> Int -> c
+fdiv = (/) `on` fromIntegral
 
 
 main :: IO ()
